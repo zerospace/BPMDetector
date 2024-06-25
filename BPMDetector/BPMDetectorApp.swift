039a471e7a79d5f86afad9813182196cf58aea89
Oleksandr Fedko <fedko.a.g@gmail.com>
2024-06-25 11:59:38 +0300

    initial commit

---
+++ BPMDetector/BPMDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMDetectorApp.swift
+//  BPMDetector
+//
+//  Created by Oleksandr Fedko on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BPMDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
